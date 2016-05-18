6ccc73bc2bf4a37100c821dabcff5faad5fe3e52
Luis Izaguirre <lsizaguirre@gmail.com>
2016-05-18 12:04:54 -0400

    First commit

---
+++ create-snapshots.py
@@ -0,0 +1,78 @@
+import boto3
+import collections
+import datetime
+
+BackupDate = datetime.date.today()
+
+ec = boto3.client('ec2', region_name='sa-east-1')
+
+#Return TagName Value
+def TagName(i):
+    for tag in i:
+        if tag[u'Key']=='Name':
+            return tag[u'Value']
+
+def lambda_handler(event, context):
+    reservations = ec.describe_instances(
+        Filters=[
+            {'Name': 'tag-key', 'Values': ['backup', 'Backup']},
+        ]
+    ).get(
+        'Reservations', []
+    )
+
+    instances = sum(
+        [
+            [i for i in r['Instances']]
+            for r in reservations
+        ], [])
+
+    print "Found %d instances that need backing up" % len(instances)
+
+    to_tag = collections.defaultdict(list)
+
+    for instance in instances:
+        try:
+            retention_days = [
+                int(t.get('Value')) for t in instance['Tags']
+                if t['Key'] == 'Retention'][0]
+        except IndexError:
+            retention_days = 2
+
+        if datetime.datetime.today().weekday() == 5:
+            retention_days = 21
+        
+        InstanceName=TagName(instance['Tags'])
+
+        for dev in instance['BlockDeviceMappings']:
+            if dev.get('Ebs', None) is None:
+                continue
+            vol_id = dev['Ebs']['VolumeId']
+            print "Found EBS volume %s on instance %s" % (
+                vol_id, instance['InstanceId'])
+
+            snap = ec.create_snapshot(
+                VolumeId=vol_id,
+                Description='Backup '+ str(BackupDate) +' Instance ' + InstanceName + ' : ' + instance['InstanceId']
+            )
+
+            to_tag[retention_days].append(snap['SnapshotId'])
+
+            print "Retaining snapshot %s of volume %s from instance %s for %d days" % (
+                snap['SnapshotId'],
+                vol_id,
+                instance['InstanceId'],
+                retention_days,
+            )
+
+
+    for retention_days in to_tag.keys():
+        delete_date = datetime.date.today() + datetime.timedelta(days=retention_days)
+        delete_fmt = delete_date.strftime('%Y-%m-%d')
+        print "Will delete %d snapshots on %s" % (len(to_tag[retention_days]), delete_fmt)
+        ec.create_tags(
+            Resources=to_tag[retention_days],
+            Tags=[
+                {'Key': 'DeleteOn', 'Value': delete_fmt},
+            ]
+        )
